92afabc19f0a81640e675227712d0ec3d436cf95
Denzel <denzeltlee@gmail.com>
2020-07-12 14:05:31 +0800

    Updates

---
+++ precache-manifest.e336befd4399bd2c8a5fe3dfa0c8aaa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1bb21a718d13c59fb787a241504482",
+    "url": "/movie-database/index.html"
+  },
+  {
+    "revision": "6a6c7f39a65f82178284",
+    "url": "/movie-database/static/css/main.d094328f.chunk.css"
+  },
+  {
+    "revision": "18c014a2cffc52f95623",
+    "url": "/movie-database/static/js/2.ac97abed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-database/static/js/2.ac97abed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6c7f39a65f82178284",
+    "url": "/movie-database/static/js/main.9944bea2.chunk.js"
+  },
+  {
+    "revision": "0203164b0f3013548c2b",
+    "url": "/movie-database/static/js/runtime-main.5cb07c8c.js"
+  },
+  {
+    "revision": "c2427492b04583164af0f709e2033860",
+    "url": "/movie-database/static/media/star.c2427492.svg"
+  }
+]);
